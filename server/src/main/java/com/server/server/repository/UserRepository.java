dbd7eede262f5a71531200003a4305a69dfc12c4
Nerilus <nerilus.h@gmail.com>
2023-10-09 22:46:43 +0200

    first commit

---
+++ server/src/main/java/com/server/server/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.server.server.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.server.server.models.User;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByUsername(String username);
+
+  Boolean existsByUsername(String username);
+
+  Boolean existsByEmail(String email);
+}
